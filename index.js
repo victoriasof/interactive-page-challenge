1920fc3716232d4b2e7205cae1a9d8538bb9d44c
becode <yf_be@yahoo.co.uk>
2020-08-12 15:03:31 +0200

    carousel working

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
 //carousel
 let carouselIndex = 1;
-displayCarousel(carouselIndex);
-
-document.getElementById('previous').addEventListener('click', previousSlide);
-document.getElementById('next').addEventListener('click', nextSlide);
-
-function nextSlide() {
-    displayCarousel(carouselIndex - 1);
-}
-
-function previousSlide() {
-    displayCarousel(carouselIndex + 1);
-}
 
 function displayCarousel(index) {
-    let paintings = document.querySelectorAll('.painting');
-    if (index > paintings.length) {
+    const paintings = document.querySelectorAll('.painting');
+    if (carouselIndex > paintings.length) {
         carouselIndex = 1
     }
-    if (index < 1) {
+    if (carouselIndex < 1) {
         carouselIndex = paintings.length
     }
     for (let i = 0; i < paintings.length; i++) {
@@ -26,6 +14,19 @@ function displayCarousel(index) {
     }
     paintings[carouselIndex - 1].style.display = 'block';
 }
+function nextSlide() {
+    carouselIndex -= 1;
+    displayCarousel(carouselIndex);
+}
+
+function previousSlide() {
+    carouselIndex += 1;
+    displayCarousel(carouselIndex);
+}
+
+displayCarousel(carouselIndex);
+document.getElementById('previous').addEventListener('click', previousSlide);
+document.getElementById('next').addEventListener('click', nextSlide);
 
 //numbers
 function countNumbers(id, max) {
@@ -40,3 +41,16 @@ function countNumbers(id, max) {
 countNumbers('number', 10);
 
 //tabs
+const tabs = document.querySelectorAll(".tab");
+const contents = document.querySelectorAll(".content");
+
+const handleTab = e => {
+    e.preventDefault();
+
+    tabs.forEach(tab => tab.classList.remove('active'));
+    e.currentTarget.classList.add('active');
+
+    /*contents.forEach(content => content.classList.remove('active'));
+    [...contents].filter(content => content.dataset.tab === e.currentTarget.dataset.tab)[0].classList.add('active');*/
+}
+tabs.forEach(tab => tab.addEventListener('click', handleTab));
